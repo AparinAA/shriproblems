8107becf35b6bc11802affd5ba1582254d82d4f7
Александр Апарин <aparinalx@yandex.ru>
2023-08-21 17:26:04 +0200

    1 problem

---
+++ 1.js
@@ -0,0 +1,23 @@
+function sortBbl(nums) {
+    const n = nums.length;
+    for (let i = 0; i < n; i++) {
+        for (let j = i + 1; j < n; j++) {
+            if (nums[j] < nums[i]) {
+                const temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+            }
+        }
+    }
+
+    return nums;
+}
+
+const tests = [
+    [3, 2, 1, 0, -1],
+    [-1, 0, 2, 3, 4, 5],
+    [1, 1],
+    [-1, 23, 5, 3, 0, -10],
+];
+
+tests.forEach((t) => console.info(sortBbl(t)));
